bb7c9f84e33ef83e9fa21dd5d4f93a1ed774940a
Fred Strauss <fred@strider.co.za>
2021-12-02 07:28:38 +0200

    Day 02

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,11 +2,49 @@ module Day02
   ( solve
   ) where
 
+data Command = Forward Int | Down Int | Up Int | InvalidCommand deriving (Eq, Show)
+
+data Submarine = Submarine
+  { horisontalPos :: Int
+  , depth         :: Int
+  , aim           :: Int
+  } deriving (Eq, Show)
+
+newSubmarine :: Submarine
+newSubmarine = Submarine 0 0 0
+
+perform :: Submarine -> Command -> Submarine
+perform s (Forward x)     = s { horisontalPos = horisontalPos s + x }
+perform s (Down    x)     = s { depth         = depth s + x }
+perform s (Up      x)     = s { depth         = depth s - x }
+perform s InvalidCommand  = s
+
+performB :: Submarine -> Command -> Submarine
+performB s (Forward x)    = s { horisontalPos = horisontalPos s + x, depth = depth s + aim s * x }
+performB s (Down    x)    = s { aim           = aim s + x }
+performB s (Up      x)    = s { aim           = aim s - x }
+performB s InvalidCommand = s
+
+parse :: String -> Command
+parse str = case v of
+              "forward" -> Forward x
+              "down"    -> Down x
+              "up"      -> Up x
+              _         -> InvalidCommand
+  where v = head $ words str
+        x = read $ words str !! 1
+
+parseInput = map parse . lines
+
 part1 :: String -> IO ()
-part1 = putStr
+part1 input = do
+  let submarine = foldl perform newSubmarine $ parseInput input
+  print $ horisontalPos submarine * depth submarine
 
 part2 :: String -> IO ()
-part2 = putStr
+part2 input = do
+  let submarine = foldl performB newSubmarine $ parseInput input
+  print $ horisontalPos submarine * depth submarine
 
 solve :: String -> IO ()
 solve input = do
