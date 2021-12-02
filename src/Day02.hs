ff2e5ccb292a403238bd2b4e7d28d6b8cda94f41
Fred Strauss <fred@strider.co.za>
2021-12-02 07:52:47 +0200

    Tidying

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,7 +2,7 @@ module Day02
   ( solve
   ) where
 
-data Command = Forward Int | Down Int | Up Int | InvalidCommand deriving (Eq, Show)
+data Command = Forward Int | Down Int | Up Int | InvalidCommand String deriving (Eq, Show)
 
 data Submarine = Submarine
   { horisontalPos :: Int
@@ -14,27 +14,27 @@ newSubmarine :: Submarine
 newSubmarine = Submarine 0 0 0
 
 perform :: Submarine -> Command -> Submarine
-perform s (Forward x)     = s { horisontalPos = horisontalPos s + x }
-perform s (Down    x)     = s { depth         = depth s + x }
-perform s (Up      x)     = s { depth         = depth s - x }
-perform s InvalidCommand  = s
+perform s (Forward x)         = s { horisontalPos = horisontalPos s + x }
+perform s (Down    x)         = s { depth         = depth s + x }
+perform s (Up      x)         = s { depth         = depth s - x }
+perform s (InvalidCommand e)  = error e
 
 performB :: Submarine -> Command -> Submarine
-performB s (Forward x)    = s { horisontalPos = horisontalPos s + x, depth = depth s + aim s * x }
-performB s (Down    x)    = s { aim           = aim s + x }
-performB s (Up      x)    = s { aim           = aim s - x }
-performB s InvalidCommand = s
+performB s (Forward x)        = s { horisontalPos = horisontalPos s + x, depth = depth s + aim s * x }
+performB s (Down    x)        = s { aim           = aim s + x }
+performB s (Up      x)        = s { aim           = aim s - x }
+performB s (InvalidCommand e) = error e
 
-parse :: String -> Command
-parse str = case v of
+parseCommand :: String -> Command
+parseCommand str = case v of
               "forward" -> Forward x
               "down"    -> Down x
               "up"      -> Up x
-              _         -> InvalidCommand
+              _         -> InvalidCommand str
   where v = head $ words str
         x = read $ words str !! 1
 
-parseInput = map parse . lines
+parseInput = map parseCommand . lines
 
 part1 :: String -> IO ()
 part1 input = do
