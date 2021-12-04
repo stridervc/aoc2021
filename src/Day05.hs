f4aecd9253db652a5cae984da965f4c81d8f133c
Fred Strauss <fred@strider.co.za>
2021-12-04 10:02:14 +0200

    Prep for day 5

---
+++ src/Day05.hs
@@ -0,0 +1,16 @@
+module Day05
+  ( solve
+  ) where
+
+part1 :: String -> IO ()
+part1 input = putStrLn "Coming soon..."
+
+part2 :: String -> IO ()
+part2 input = putStrLn "Coming soon..."
+
+solve :: String -> IO ()
+solve input = do
+  putStr "Part 1 : "
+  part1 input
+  putStr "Part 2 : "
+  part2 input
