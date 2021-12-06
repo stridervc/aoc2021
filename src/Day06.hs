02f9967bb84319b227a13577ec5a0011fdae1563
Fred Strauss <fred@strider.co.za>
2021-12-06 17:59:16 +0200

    Caved and did part 1 with part 2's method

--- src/Day06.hs
+++ src/Day06.hs
@@ -9,25 +9,6 @@ import Data.List.Split (splitOn)
 parseInput :: String -> [Int]
 parseInput input = map read $ splitOn "," input
 
--- Part 1 --
--- part 1 can be rewritten with part 2 solution, but I'm leaving my quick and dirty solution as is
-
-advance :: [Int] -> [Int]
-advance []      = []
-advance (x:xs)
-  | x == 0    = [6,8] ++ advance xs
-  | otherwise = x - 1 : advance xs
-
--- apply f, x number of times to a
-applyX :: Int -> (a -> a) -> a -> a
-applyX 0 _ a = a
-applyX x f a = applyX (x-1) f (f a)
-
-part1 :: String -> IO ()
-part1 input = print $ length $ applyX 80 advance $ parseInput input
-
--- Part 2 --
-
 -- count how many fish there are with each time remaining (0 to 8)
 countTimers :: [Int] -> [Int]
 countTimers fish = map count [0..8]
@@ -36,6 +17,20 @@ countTimers fish = map count [0..8]
 advanceTimers :: [Int] -> [Int]
 advanceTimers tfs = map (\t -> count (t+1)) [0..5] ++ [count 7 + count 0, count 8, count 0]
   where count t = tfs!!t
+
+-- apply f, x number of times to a
+applyX :: Int -> (a -> a) -> a -> a
+applyX 0 _ a = a
+applyX x f a = applyX (x-1) f (f a)
+
+-- Part 1 --
+
+part1 :: String -> IO ()
+part1 input = do
+  let advanced = applyX 80 advanceTimers $ countTimers $ parseInput input
+  print $ sum advanced
+
+-- Part 2 --
 
 part2 :: String -> IO ()
 part2 input = do
