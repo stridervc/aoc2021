6c56673e8cfcffa98138a459886a4f031b50acff
Fred Strauss <fred@strider.co.za>
2021-12-06 07:40:44 +0200

    Day 6 done

--- src/Day06.hs
+++ src/Day06.hs
@@ -4,15 +4,45 @@ module Day06
 
 import Helpers
 
+import Data.List.Split (splitOn)
+
+parseInput :: String -> [Int]
+parseInput input = map read $ splitOn "," input
+
 -- Part 1 --
 
+advance :: [Int] -> [Int]
+advance []      = []
+advance (x:xs)
+  | x == 0    = [6,8] ++ advance xs
+  | otherwise = x - 1 : advance xs
+
+-- apply f x number of times to a
+applyX :: Int -> (a -> a) -> a -> a
+applyX 0 _ a = a
+applyX x f a = applyX (x-1) f (f a)
+
 part1 :: String -> IO ()
-part1 input = putStrLn "Coming soon..."
+part1 input = do
+  print $ length $ applyX 80 advance $ parseInput input
 
 -- Part 2 --
 
+type Timer = Int
+
+-- count how many fish there are with each time remaining (0 to 8)
+countTimers :: [Timer] -> [(Timer,Int)]
+countTimers fish = map (\t -> (t, count t)) [0..8]
+  where count t = length $ filter (==t) fish
+
+advanceTimers :: [(Timer,Int)] -> [(Timer,Int)]
+advanceTimers tfs = map (\t -> (t, count (t+1))) [0..5] ++ [(6,count 7 + count 0)] ++ [(7,count 8)] ++ [(8,count 0)]
+  where count t = snd $ tfs!!t
+
 part2 :: String -> IO ()
-part2 input = putStrLn "Coming soon..."
+part2 input = do
+  let advanced = applyX 256 advanceTimers $ countTimers $ parseInput input
+  print $ sum $ map snd advanced
 
 solve :: String -> IO ()
 solve input = do
