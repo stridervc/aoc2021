daea8232e9ad90a0b61136d31e5e7421594afad9
Fred Strauss <fred@strider.co.za>
2021-12-07 07:33:05 +0200

    Day 7 done

--- src/Day07.hs
+++ src/Day07.hs
@@ -3,20 +3,33 @@ module Day07
   ) where
 
 import Helpers
+import Data.List.Split (splitOn)
+
+parseInput :: String -> [Int]
+parseInput input = map read $ splitOn "," input
 
 -- Part 1 --
 
-part1 :: String -> IO ()
-part1 input = putStrLn "Coming soon..."
+fuelForPos :: [Int] -> Int -> Int
+fuelForPos crabs pos = sum $ map (\c -> abs (pos-c)) crabs
 
 -- Part 2 --
 
-part2 :: String -> IO ()
-part2 input = putStrLn "Coming soon..."
+fuelForPos2 :: [Int] -> Int -> Int
+fuelForPos2 crabs pos = sum $ map cost crabs
+  where d c     = abs $ pos-c
+        cost c  = sum $ take (d c) [1..]
 
 solve :: String -> IO ()
 solve input = do
+  let input'  = parseInput input
+  let min'    = minimum input'
+  let max'    = maximum input'
+
   putStr "Part 1 : "
-  part1 input
+  let fuels = map (fuelForPos input') [min'..max']
+  print $ snd $ head $ sortOn snd $ zip [min'..max'] fuels
+
   putStr "Part 2 : "
-  part2 input
+  let fuels2 = map (fuelForPos2 input') [min'..max']
+  print $ snd $ head $ sortOn snd $ zip [min'..max'] fuels2
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,9 +1,13 @@
 module Helpers
   ( parseInt
+  , sort
+  , sortOn
   ) where
 
 import qualified Text.Parsec as P
 import Text.Parsec.String (Parser)
+
+import Data.List (sort, sortOn)
 
 parseInt :: Parser Int
 parseInt = do
