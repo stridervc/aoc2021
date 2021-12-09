c1e7c906d8d0669d515c40c867cd38b8a69702af
Fred Strauss <fred@strider.co.za>
2021-12-09 16:59:58 +0200

    Improve template

--- src/Day10.hs
+++ src/Day10.hs
@@ -4,19 +4,24 @@ module Day10
 
 import Helpers
 
+type Input = String
+
+parseInput :: String -> Input
+parseInput input = input
+
 -- Part 1 --
 
-part1 :: String -> IO ()
+part1 :: Input -> IO ()
 part1 input = putStrLn "Coming soon..."
 
 -- Part 2 --
 
-part2 :: String -> IO ()
+part2 :: Input -> IO ()
 part2 input = putStrLn "Coming soon..."
 
 solve :: String -> IO ()
 solve input = do
   putStr "Part 1 : "
-  part1 input
+  part1 $ parseInput input
   putStr "Part 2 : "
-  part2 input
+  part2 $ parseInput input
