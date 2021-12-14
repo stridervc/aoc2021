861530c12bf96f6ff265403d3c5b0f7182377392
Fred Strauss <fred@strider.co.za>
2021-12-14 09:47:21 +0200

    Day 14 done

--- src/Day14.hs
+++ src/Day14.hs
@@ -7,10 +7,14 @@ import qualified Text.Parsec as P
 import Text.Parsec.String (Parser)
 import Text.Parsec ((<|>))
 import Data.List (group)
+import qualified Data.Map.Strict as M
 
-type Polymer  = String
-type Pair     = (Char,Char)
-type Rule     = (Pair,Char)
+type Polymer    = String
+type Pair       = (Char,Char)
+type Pairs      = [Pair]
+type Rule       = (Pair,Char)
+type Rules      = [Rule]
+type PairCounts = M.Map Pair Int
 
 parsePolymer :: Parser Polymer
 parsePolymer = P.many1 P.letter
@@ -31,41 +35,58 @@ parseAll = do
   rules <- P.endBy parseRule (P.spaces <|> P.eof)
   return (polymer,rules)
 
-type Parsed = (Polymer,[Rule])
+type Parsed = (PairCounts,Rules)
+
+addPair :: Int -> PairCounts -> Pair -> PairCounts
+addPair n pcs pair = M.alter f pair pcs
+  where f Nothing   = Just n
+        f (Just c)  = Just $ n + c
 
 parseInput :: String -> Parsed
 parseInput input = do
-  let Right parsed = P.parse parseAll "(input)" input
-  parsed
+  let Right (polymer,rules) = P.parse parseAll "(input)" input
+  let paired      = [ (polymer!!(i-1),polymer!!i) | i <- [1..length polymer - 1] ]
+  let countpairs  = foldl (addPair 1) mempty paired
+  (countpairs, rules)
 
--- perform insertion of a single pair
-performInsert :: [Rule] -> Pair -> String
-performInsert rules pair@(c1,c2)
-  | null lookupRule = [c1,c2]
-  | otherwise       = [c1, snd lookupRule, c2]
+-- a pair will split into two pairs, if there's a rule that matches the pair
+splitPair :: Rules -> Pair -> [Pair]
+splitPair rules pair@(c1,c2)
+  | null lookupRule = [pair]
+  | otherwise       = [pair1, pair2]
   where lookupRule  = head $ filter (\r -> fst r == pair) rules
+        pair1       = (c1, snd lookupRule)
+        pair2       = (snd lookupRule, c2)
 
 -- perform one set of insertions
-step :: [Rule] -> Polymer -> Polymer
-step rules polymer = concatMap init inserted ++ [last (last inserted)]
-  where pairs     = [ (polymer!!(i-1),polymer!!i) | i <- [1..length polymer - 1] ]
-        inserted  = map (performInsert rules) pairs
+step :: Rules -> PairCounts -> PairCounts
+step rules = M.foldlWithKey f mempty
+  where f newpcs k c  = foldl (addPair c) newpcs $ splitPair rules k
 
-stepN :: Int -> [Rule] -> Polymer -> Polymer
-stepN n rules polymer = last $ take n $ tail $ iterate (step rules) polymer
+stepN :: Int -> Rules -> PairCounts -> PairCounts
+stepN n rules pcs = last $ take n $ tail $ iterate (step rules) pcs
+
+countChars :: PairCounts -> [(Char,Int)]
+countChars pcs = [ (ch,(countat fst ch + countat snd ch) `div` 2) | ch <- allchars ]
+  where keys          = M.keys pcs
+        allchars      = nub $ map fst keys <> map snd keys
+        countat f ch  = sum $ map (\(k,v) -> if f k == ch then v else 0) $ M.toList pcs
 
 -- Part 1 --
 
--- 3408 in 0m2.939s
 part1 :: Parsed -> IO ()
-part1 (polymer,rules) = do
-  let counts = map length $ group $ sort $ stepN 10 rules polymer
-  print $ maximum counts - minimum counts
+part1 (countpairs,rules) = do
+  let stepped = stepN 10 rules countpairs
+  let counts = sort $ map snd $ countChars stepped
+  print $ last counts - head counts
 
 -- Part 2 --
 
 part2 :: Parsed -> IO ()
-part2 (polymer,rules) = putStrLn "Coming soon.."
+part2 (countpairs,rules) = do
+  let stepped = stepN 40 rules countpairs
+  let counts = sort $ map snd $ countChars stepped
+  print $ last counts - head counts
 
 solve :: String -> IO ()
 solve input = do
